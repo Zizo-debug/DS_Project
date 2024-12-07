cd10bfb73bc54f200b72f5997dbc8762865b99a6
azfaar1 <azfaars12@gmail.com>
2024-12-07 18:16:51 +0500

    add missing nl

--- q1.cpp
+++ q1.cpp
@@ -14,6 +14,7 @@ class Restaurants;
 bool isValidPassword(const string& password) {
     if (password.length() < 8 || password.length() > 12) {
         cout << "Password must be within 8 and 12 characters long.\n";
+
         return false;
     }
 
